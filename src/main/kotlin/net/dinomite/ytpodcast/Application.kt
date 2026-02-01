ca1426074298a7fc1bd9d07dd4d7004c9bdf6fd7
Drew Stephens <drew@dinomite.net>
2026-02-01 10:19:16 -0500

    Strip down to basics for restarting with Claude

--- src/main/kotlin/net/dinomite/ytpodcast/Application.kt
+++ src/main/kotlin/net/dinomite/ytpodcast/Application.kt
@@ -3,14 +3,10 @@ package net.dinomite.ytpodcast
 import io.ktor.server.application.Application
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
-import net.dinomite.ytpodcast.controller.PlaylistController
 import net.dinomite.ytpodcast.plugins.configureHTTP
 import net.dinomite.ytpodcast.plugins.configureMonitoring
 import net.dinomite.ytpodcast.plugins.configureRouting
 import net.dinomite.ytpodcast.plugins.configureSerialization
-import net.dinomite.ytpodcast.service.AudioConverterService
-import net.dinomite.ytpodcast.service.RssFeedService
-import net.dinomite.ytpodcast.service.YouTubeService
 
 fun main() {
     embeddedServer(
@@ -22,19 +18,9 @@ fun main() {
 }
 
 fun Application.module() {
-    // Initialize services
-    val youTubeService = YouTubeService()
-    val audioConverterService = AudioConverterService()
-    val rssFeedService = RssFeedService()
-    val playlistController = PlaylistController(
-        youTubeService,
-        rssFeedService,
-        audioConverterService
-    )
-
     // Configure plugins
     configureSerialization()
     configureMonitoring()
     configureHTTP()
-    configureRouting(playlistController)
+    configureRouting()
 }
